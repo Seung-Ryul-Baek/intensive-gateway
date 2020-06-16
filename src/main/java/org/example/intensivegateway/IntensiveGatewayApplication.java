d10aedef3639ffae2bc1b4fa3ca1e8bd24f602f6
hwang-mh <hwangmh@sk.com>
2020-06-16 16:51:01 +0900

    first commit

---
+++ src/main/java/org/example/intensivegateway/IntensiveGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.intensivegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensiveGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensiveGatewayApplication.class, args);
+	}
+
+}
